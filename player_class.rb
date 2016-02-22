6a8fe71d37f810112aa420329a8c41d51144a54a
Jake Day Williams <jake@wildacademy.com>
2016-02-22 18:55:40 +0000

    player is go

--- player_class.rb
+++ player_class.rb
@@ -1,19 +1,39 @@
 class Player
-  attr_accessor :wallet
+  attr_accessor :wallet_add, :wallet_subtract, :check_wallet
   attr_reader :name
+  
   def initialize(name,wallet)
     @name = name
-    @wallet = wallet
+    @wallet = (wallet)
   end
 
+  def check_wallet
+    puts @wallet
+  end
 
-def check_wallet
-  puts wallet
+  def wallet_add(bet)
+    @wallet = @wallet + bet
+  end
+  
+  def wallet_subtract(bet)
+    @wallet = @wallet - bet
+  end
+
 end
 
-end
-
-jake = Player.new("Jake",40.00)
-jake.check_wallet
 
 
+# ------THIS IS FOR TESTING-------
+
+
+# jake = Player.new("Jake",40.00)
+# jake.check_wallet
+
+# jake.wallet_add(10.00)
+# jake.check_wallet
+
+
+# jake.wallet_subtract(10.00)
+# jake.check_wallet
+
+# ---------TESTING END------------
