79e6ea92a174f8558b727de0c214b01d741ef022
Jake Day Williams <jake@wildacademy.com>
2016-02-23 22:19:19 +0000

    Add casino_menu

---
+++ casino_menu.rb
@@ -0,0 +1,18 @@
+require 'colorize'
+
+print "                ".on_white
+print "C ".bold.red.on_white
+print "A ".bold.yellow.on_white
+print "S ".bold.green.on_white
+print "I ".bold.cyan.on_white
+print "N ".bold.blue.on_white
+print "O".bold.magenta.on_white
+puts "                  ".on_white
+puts " .-------.                         .-------. ".bold.on_red
+puts " |A  .   |  1) GAMES               |2 /\\   | ".bold.on_yellow
+puts " |  / \\  |  2) CHECK BALANCE       |  \\/   | ".bold.on_green
+puts " | (_,_) |  3) ADD ANOTHER PLAYER  |       | ".bold.on_cyan
+puts " |   I   |  4) SWITCH PLAYERS      |  /\\   | ".bold.on_blue
+puts " |      A|  5) EXIT                |  \\/  2| ".bold.on_magenta
+puts " `-------+                         `-------+ ".bold.on_red
+puts "                                             ".on_white
